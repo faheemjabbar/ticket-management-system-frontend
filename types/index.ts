b4de93ed23fcd8a40d338c8d5519b1ec8a521515
faheemjabbar <faheemjabbar36@gmail.com>
2026-02-12 17:17:44 +0500

    fix: resolve Organization type export ambiguity

--- types/index.ts
+++ types/index.ts
@@ -2,3 +2,7 @@ export * from './user.types';
 export * from './ticket.types';
 export * from './comment.types';
 export * from './project.types';
+export * from './organization.types';
+
+// Re-export Organization from organization.types to avoid ambiguity
+export type { Organization } from './organization.types';
